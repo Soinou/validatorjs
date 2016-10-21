1014b02b75eb09535fd49500ec4f6b54c9090607
Soinou <abricot.soinou@gmail.com>
2016-10-21 16:47:06 +0200

    Fixed french translation

--- src/lang/fr.js
+++ src/lang/fr.js
@@ -1,34 +1,34 @@
 module.exports = {
-  accepted: 'Le champs :attribute doit être accepté.',
-  alpha: 'Le champs :attribute ne peut contenir que des caractères alphabétiques.',
-  alpha_dash: 'Le champs :attribute ne peut contenir que des caractères alphanumériques, des tirets et underscores.',
-  alpha_num: 'Le champs :attribute doit être alphanumérique.',
-  between: 'Le champs :attribute doit être compris entre :min and :max.',
-  confirmed: 'Le champs :attribute ne correspond pas.',
-  email: 'Le champs :attribute contient un format invalide.',
-  def: 'Le champs :attribute contient un attribut erroné.',
-  digits: 'Le champs :attribute doit être de :digits chiffres.',
-  different: 'Le champs :attribute et :different doivent être differents.',
-  'in': 'Le champs :attribute est invalide.',
-  integer: 'Le champs :attribute doit être un entier.',
+  accepted: 'Le champ :attribute doit être accepté.',
+  alpha: 'Le champ :attribute ne peut contenir que des caractères alphabétiques.',
+  alpha_dash: 'Le champ :attribute ne peut contenir que des caractères alphanumériques, des tirets et underscores.',
+  alpha_num: 'Le champ :attribute doit être alphanumérique.',
+  between: 'Le champ :attribute doit être compris entre :min and :max.',
+  confirmed: 'Le champ :attribute ne correspond pas.',
+  email: 'Le champ :attribute contient un format invalide.',
+  def: 'Le champ :attribute contient un attribut erroné.',
+  digits: 'Le champ :attribute doit être de :digits chiffres.',
+  different: 'Le champ :attribute et :different doivent être differents.',
+  'in': 'Le champ :attribute est invalide.',
+  integer: 'Le champ :attribute doit être un entier.',
   min: {
-    numeric: 'Le champs :attribute doit être contenir au moins :min.',
-    string: 'Le champs :attribute doit être contenir au moins :min caractères.'
+    numeric: 'Le champ :attribute doit être supérieur à :min.',
+    string: 'Le champ :attribute doit contenir au moins :min caractères.'
   },
   max: {
-    numeric: 'Le champs :attribute ne doit être supérieur à :max.',
-    string: 'Le champs :attribute ne doit être plus de :max characters.'
+    numeric: 'Le champ :attribute doit être inférieur à :max.',
+    string: 'Le champ :attribute doit contenir au plus :max caractères.'
   },
-  not_in: 'Le champs :attribute est invalide.',
-  numeric: 'Le champs :attribute doit être un numéro.',
-  required: 'Le champs :attribute est obligatoire.',
-  required_if: 'Le champs :attribute est obligatoire quand :other est :value.',
-  same: 'Le champs :attribute et :same doivent correspondre.',
+  not_in: 'Le champ :attribute est invalide.',
+  numeric: 'Le champ :attribute ne peut contenir que des caractères numériques.',
+  required: 'Le champ :attribute est obligatoire.',
+  required_if: 'Le champ :attribute est obligatoire quand :other est :value.',
+  same: 'Les champs :attribute et :same doivent correspondre.',
   size: {
-    numeric: 'La taille du champs :attribute doit être :size.',
-    string: 'La taille du champs :attribute doit être de :size caractères.'
+    numeric: 'Le nombre :attribute doit contenir :size chiffres.',
+    string: 'Le champ :attribute contenir exactement :size caractères.'
   },
-  url: 'Le format du champs :attribute est invalide.',
-  regex: 'Le format du champs :attribute est invalide.',
+  url: 'Le format du champ :attribute est invalide.',
+  regex: 'Le format du champ :attribute est invalide.',
   attributes: {}
 };
